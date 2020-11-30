6f487c46ee0581cea57c667b8bfe778c78349146
emredemirel41 <emredemirel4196@gmail.com>
2020-11-30 04:22:50 +0300

    Case Project Version 1-RESTFUL Project

---
+++ snippets/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+
+
+class Snippet(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    value = models.CharField(max_length=100)
+    owner = models.ForeignKey('auth.User', related_name='snippets', on_delete=models.CASCADE)
+    class Meta:
+        ordering = ['created']
+
+    def save(self, *args, **kwargs):
+        super(Snippet, self).save(*args, **kwargs)
